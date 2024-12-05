edd6cd9a9392cf077251261981e1c6a5bd2c3ce6
Corey Finchum <corey.finchum@gmail.com>
2024-12-04 20:35:21 -0500

    replaced all files with content from main

--- app.py
+++ app.py
@@ -1,5 +1,46 @@
-from flask import Flask, request, render_template
-import ee
+from flask import Flask, request, render_template,jsonify
+import ee, math
+
+
+def carbon_stock_change(t, max_biomass, growth_rate, mortality_rate, root_to_shoot, areas, baseline):
+    # Calculates the change in carbon stocks at year t compared to year t=0
+
+    # Calculate the average carbon stock (tC/ha) in aboveground woody biomass in the project scenario in year t
+    aboveground_woody_biomass = calculate_aboveground_woody_biomass(t, max_biomass, growth_rate, mortality_rate)
+    aboveground_woody_biomass_carbon_stock_avg = aboveground_woody_biomass * 0.47 # Carbon makes up approximately 47% of the dry biomass for most tree species
+
+    # Calculate the average carbon stock (tC/ha) in woody biomass in the project scenario in year t
+    woody_biomass_carbon_stock_avg = aboveground_woody_biomass_carbon_stock_avg * (1 + root_to_shoot)
+
+    # Calculate the total carbon stocks (tC) of the reforested areas in year t
+    converted_area = areas["Shrubland"] + areas["Grassland"] + areas["Cropland"]
+    total_carbon_stocks = woody_biomass_carbon_stock_avg * converted_area
+
+    # Calculate and return change in carbon stocks
+    change_in_carbon_stocks = total_carbon_stocks - baseline
+    return change_in_carbon_stocks
+
+
+def calculate_aboveground_woody_biomass(t, max_biomass, growth_rate, mortality_rate=0.1):
+    # Caculates the aboveground woody biomass (t/ha) in the project scenario at year t
+
+    # Logistic growth model to estimate biomass accumulation
+    biomass = max_biomass * (1 - math.exp(-growth_rate * t))
+
+    # Adjust biomass for mortality rate
+    adjusted_biomass = biomass * (1 - mortality_rate)
+
+    return adjusted_biomass
+
+def calculate_baseline(areas):
+    # Calculates the baseline carbon stocks (tC) of the area to be converted to tree cover by getting
+    # the sum of the products of the areas of each land feature to be converted and their respective average carbon stocks (tC/ha)
+    shrubland_carbon_stocks = landcover_classes[20]["avg_carbon_stocks"] * areas["Shrubland"]
+    grassland_carbon_stocks = landcover_classes[30]["avg_carbon_stocks"] * areas["Grassland"]
+    cropland_carbon_stocks = landcover_classes[40]["avg_carbon_stocks"] * areas["Cropland"]
+    baseline = shrubland_carbon_stocks + grassland_carbon_stocks + cropland_carbon_stocks
+    return baseline
+
 
 def calculate_carbon_stocks(areas):
 # Calculates carbon stocks of project area using averages for each land feature
@@ -29,15 +70,15 @@ except ee.EEException as e:
 
 # Define land cover classes with their corresponding colors and average carbon stocks per hectare (tC/ha)
 landcover_classes = {
-    10: {"name": "Tree Cover", "color": "#006400", "avg_carbon_stocks": 350},               # Dark Green
-    20: {"name": "Shrubland", "color": "#228B22", "avg_carbon_stocks": 110},                # Forest Green
-    30: {"name": "Grassland", "color": "#7CFC00", "avg_carbon_stocks": 65},                 # Lawn Green
-    40: {"name": "Cropland", "color": "#FFD700", "avg_carbon_stocks": 58},                  # Gold
-    50: {"name": "Built-up", "color": "#A9A9A9", "avg_carbon_stocks": 20},                  # Dark Gray
-    60: {"name": "Bare / Sparse Vegetation", "color": "#DEB887", "avg_carbon_stocks": 11},  # Burly Wood
+    10: {"name": "Tree Cover", "color": "#006400", "avg_carbon_stocks": 25},               # Dark Green
+    20: {"name": "Shrubland", "color": "#228B22", "avg_carbon_stocks": 10},                # Forest Green
+    30: {"name": "Grassland", "color": "#7CFC00", "avg_carbon_stocks": 4},                 # Lawn Green
+    40: {"name": "Cropland", "color": "#FFD700", "avg_carbon_stocks": 5},                  # Gold
+    50: {"name": "Built-up", "color": "#A9A9A9", "avg_carbon_stocks": 0.1},                  # Dark Gray
+    60: {"name": "Bare / Sparse Vegetation", "color": "#DEB887", "avg_carbon_stocks": 0.5},  # Burly Wood
     70: {"name": "Snow and Ice", "color": "#FFFFFF", "avg_carbon_stocks": 0},               # White
     80: {"name": "Permanent Water Bodies", "color": "#1E90FF", "avg_carbon_stocks": 0},     # Dodger Blue
-    90: {"name": "Herbaceous Wetland", "color": "#00CED1", "avg_carbon_stocks": 260},       # Dark Turquoise
+    90: {"name": "Herbaceous Wetland", "color": "#00CED1", "avg_carbon_stocks": 25},       # Dark Turquoise
 }
 
 @app.route('/')
@@ -47,7 +88,6 @@ def index():
 @app.route('/submit_coordinates', methods=['POST'])
 def submit_coordinates():
     data = request.form
-
     try:
         # Extract coordinates from user input
         top_left_lat = float(data['top_left_latitude'])
@@ -103,69 +143,17 @@ def submit_coordinates():
             # Convert the area to hectares (1 hectare = 10,000 m²)
             areas[class_name] = area['Map'] / 10000 if area['Map'] else 0
 
-        # Calculate carbon stocks (tC) of current project area
-        current_carbon_stocks = calculate_carbon_stocks(areas)
+        baseline = calculate_baseline(areas)
 
-        # Calculate potential carbon stocks (tC) of reforested project area
-        reforested_areas = reforest(areas)
-        reforested_carbon_stocks = calculate_carbon_stocks(reforested_areas)
-
-        # Calculate additionality (tC) and from reforestation project
-        additionality = reforested_carbon_stocks - current_carbon_stocks 
-
-        # Calculate number of carbon credits earned from reforestation project
-        # 1 tonne of carbon (tC) corresponds to 3.667 carbon credits in terms of CO2 equivalent
-        carbon_credits = additionality * 3.667
-        print(carbon_credits)
-
-        # BEGIN TIME SERIES ANALYSIS
-        # Define new classes for the COPERNICUS data
-        landcover_classes_new = {
-            1: {"name": "Saturated or defective", "color": "#ff0004"},   
-            2: {"name": "Dark Area Pixels", "color": "#868686"},               
-            3: {"name": "Cloud Shadows", "color": "#774b0a"},    
-            4: {"name": "Vegetation", "color": "#10d22c"},          
-            5: {"name": "Bare Soils", "color": "#ffff52"},             
-            6: {"name": "Water", "color": "#0000ff"},
-            7: {"name": "Clouds Low Probability", "color": "#818181"}
-        }
-
-        # Import image collection for each year from Sentinel-2 data
-        areas_collection = {}
-        for year in range(2015, 2024):
-            landcover_collection_image = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED") \
-                .filter(ee.Filter.date(ee.Date.fromYMD(year, 1, 1), ee.Date.fromYMD(year, 1, 28))) \
-                .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 30)) \
-                .filterBounds(rectangle)
-
-            # Take a median composite to reduce noise
-            landcover_collection_image = landcover_collection_image.median()
-
-            # Get yearly area
-            areas_image = {}
-            for class_value, class_info in landcover_classes_new.items():
-                class_name = class_info["name"]
-
-                
-                # Check if the image has any bands before proceeding
-                if landcover_collection_image.bandNames().size().getInfo() == 0:
-                    areas_image[class_name] = 0
-                    continue
-
-                # Mask the image to isolate the current class
-                masked_class = landcover_collection_image.eq(class_value)
-
-                # Calculate area in square meters
-                area_image = masked_class.multiply(ee.Image.pixelArea()).reduceRegion(
-                    reducer=ee.Reducer.sum(),
-                    geometry=rectangle,
-                    scale=10,
-                    maxPixels=1e9
-                ).getInfo()
-
-                # Convert the area to hectares (1 hectare = 10,000 m²)
-                areas_image[class_name] = area_image['SCL'] / 10000 if area_image['SCL'] else 0
-            areas_collection[year] = areas_image
+        # Calculate carbon stock changes (tC) in years 1-10
+        carbon_stock_changes = []
+        for year in range(1, 11):
+            carbon_stock_changes.append(carbon_stock_change(year, 150, 0.1, 0.1, 0.25, areas, baseline))
+        
+        # Calculate carbon credits earned in years 1-10
+        carbon_credits_earned = []
+        for year in range(10):
+            carbon_credits_earned.append(carbon_stock_changes[year] * (44/12))
 
         return render_template('results.html',
                                image_url=image_url,
@@ -176,16 +164,16 @@ def submit_coordinates():
                                right_lon=right_lon,
                                landcover_classes=landcover_classes,
                                landcover_areas=areas,
-                               current_carbon_stocks=current_carbon_stocks,
-                               reforested_carbon_stocks=reforested_carbon_stocks,
-                               additionality=additionality,
-                               carbon_credits=carbon_credits,
-                               areas_collection=areas_collection,
-                               landcover_classes_new = landcover_classes_new
+                               carbon_stock_changes=carbon_stock_changes,
+                               carbon_credits_earned=carbon_credits_earned
                                )
     except Exception as e:
-        print("Error processing coordinates:", e)
-        return render_template('error.html', error_message='An error occurred while processing the coordinates.')
+        # print("Error processing coordinates:", e)
+        # return render_template('error.html', error_message='An error occurred while processing the coordinates.')
+        import traceback
+        print("Error processing coordinates:", str(e))
+        print("Full traceback:", traceback.format_exc())
+        return render_template('error.html', error_message=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
